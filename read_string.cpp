65f4449d26624c407ee56e10e7c0c24748a13148
AlekseyZarezin <AlekseyZarezin@yandex.ru>
2016-05-21 19:15:35 +0300

    Проверка на корректность скобок

--- read_string.cpp
+++ read_string.cpp
@@ -4,29 +4,108 @@
 #include<math.h>
 #define CASH 30
 
+struct node
+{
+    char data;
+    node * next;
+};
+
+struct stech
+{
+    node * head;
+    void   init(){head = NULL;}
+    char push(char data)
+    {        
+        if (deep()>0&&(data==')'))
+        {
+        	if (data==')'&&head->data=='(')
+        	{
+				pop();
+				return 1;
+			}
+			else
+				return 0;
+		}
+        else
+		{  
+			node *n=(node *)malloc(sizeof(node));  	
+        	n->data=data;
+        	n->next=head;    
+        	head = n; 
+        	return 1;
+		}
+    }
+    void pop()
+    {
+        node *h=head;
+        head=(*head).next;
+        free(h);
+    }
+    int deep()
+    {
+        int i=0;
+        node *h=head;
+        while (h!=NULL)
+        {
+            i++;
+            h=h->next;
+        }
+        return i;
+    }
+    void clean()                  //очистка памяти
+    {
+    	while (deep()!=0)
+    	{
+    		node *h=head;
+        	head=head->next;
+        	free(h);
+		}
+		head = NULL;
+	}
+};
+
 int main(void)
 {
 	int N, j=0, i=0, l=CASH, n;
-	char c, *m;   
-	m = (char*)malloc(CASH*sizeof(char));                //ввод строки    
+	char c, *s;   
+	s = (char*)malloc(CASH*sizeof(char));                //ввод строки    
 	i=0;
 	c=getchar();
 	while (c!='\n')
 	{
-		m[i]=c;
+		s[i]=c;
 		i++;
 		c=getchar();
 		if (i+2==l)
 		{
 			l+=CASH;
-			m=(char*)realloc(m, l*sizeof(char));
+			s=(char*)realloc(s, l*sizeof(char));
 		}
 	}
-	m[i]='\0';	                                           //ввод строки
-	int length = i;
+	s[i]='\0';	                                           
+	int length = i;	
+	stech st;                              //проверка на корректность
+    st.init();
+    char f=1;
+	for (int i=0; i<length; i++)
+	{
+		if (((s[i])=='(')||((s[i])==')'))
+			if (!st.push(s[i]))
+			{
+				f=0;
+				break;
+			}
+	}
+	if (st.deep()>0)
+		f=0;
+	if (f)
+		printf("yes\n");
+	else
+		printf("no\n");
+	st.clean();
 	
-	printf("%s\n", m);
+	//printf("%s\n", s);
 	system("pause");
-	free(m);
+	free(s);
 	return 0;
 }
